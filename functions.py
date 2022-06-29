365a1819172f1515fc6396603d3d50e86b3ee761
Esteban Tinoco <esteban.tinoco7@gmail.com>
2022-06-29 16:29:18 -0500

    Add files via upload
    Added functions.py file

---
+++ functions.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import streamlit as st
+
+# Function to extract data from tables, concat the list of data together to make a df and then drop unnecessary data from df
+@st.cache
+def get_data(index_to_drop, css_selector, columns_to_drop):
+    table = pd.read_html("https://www.basketball-reference.com/players/c/curryst01.html", attrs={"id": css_selector})
+    concat_table = pd.concat(table)
+    df = concat_table.drop(index=index_to_drop, columns=columns_to_drop)
+    
+    return df
